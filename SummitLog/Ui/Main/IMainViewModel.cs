1fe709bca3c8f787e9d4d25f1a09a548f2a4c30a
Bernd Boruttau <berndnet2000@web.de>
2014-02-13 18:09:18 +0100

    Views angelegt

---
+++ SummitLog/Ui/Main/IMainViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SummitLog.Ui.MainView {
+    /// <summary>
+    /// Schnittstelle für View Models des Hauptfensters <see cref="MainView"/>
+    /// </summary>
+    public interface IMainViewModel {
+         
+    }
+}
---
+++ SummitLog/Ui/Main/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SummitLog.Ui.MainView {
+    /// <summary>
+    /// Interaktionslogik für MainView.xaml
+    /// </summary>
+    public partial class MainView : Window {
+        public MainView() {
+            InitializeComponent();
+        }
+    }
+}
