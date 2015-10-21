0c8b9a2d398ecd55c6eb27d3881a0f08003fc861
Bernd Boruttau <berndnet2000@web.de>
2015-10-21 14:58:01 +0200

    Area Service

---
+++ SummitLog/SummitLog.Services.Test/ServiceTests/AreaServiceTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+
+using Moq;
+using NUnit.Framework;
+using SummitLog.Services.Model;
+using SummitLog.Services.Persistence;
+using SummitLog.Services.Services;
+using SummitLog.Services.Services.Impl;
+
+namespace SummitLog.Services.Test.ServiceTests
+{
+    [TestFixture]
+    public class AreaServiceTest
+    {
+        [Test]
+        public void TestGetAll()
+        {
+            Mock<IAreaDao> areaDaoMock = new Mock<IAreaDao>();
+            areaDaoMock.Setup(x => x.GetAllIn(It.IsAny<Country>())).Returns(new List<Area> {new Area {Name = "Gebiet 1"}});
+
+            Country fakeCountry = new Country() {Name = "D"};
+
+            IAreaService areaService = new AreaService(areaDaoMock.Object);
+            IList<Area> areasInCountry = areaService.GetAllIn(fakeCountry);
+            Assert.AreEqual(1, areasInCountry.Count);
+
+            areaDaoMock.Verify(x=>x.GetAllIn(It.Is<Country>(y=>y.Name == fakeCountry.Name)));
+        }
+
+        [Test]
+        public void TestCreate()
+        {
+            Mock<IAreaDao> areaDaoMock = new Mock<IAreaDao>();
+            areaDaoMock.Setup(x => x.Create(It.IsAny<Country>(), It.IsAny<Area>()));
+
+            string countryName = "D";
+            string areaName = "Gebiet 1";
+            Country fakeCountry = new Country() { Name = countryName };
+
+            IAreaService areaService = new AreaService(areaDaoMock.Object);
+            areaService.Create(fakeCountry, areaName);
+
+            areaDaoMock.Verify(x=>x.Create(It.Is<Country>(y=>y.Name == countryName), It.Is<Area>(y=>y.Name == areaName)), Times.Once);
+        }
+
+        [TestCase(true,"")]
+        [TestCase(true, " ")]
+        [TestCase(true, "    ")]
+        [TestCase(true, null)]
+        [TestCase(false, "Gebiet 1")]
+        public void TestCreateMissingName(bool useCountry, string name)
+        {
+            Country fakeCountry = null;
+            if (useCountry)
+            {
+                fakeCountry = new Country();
+            }
+
+            Action act = ()=>new AreaService(null).Create(fakeCountry, name);
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TestGetAllWithoutCountry()
+        {
+            Action act = ()=> new AreaService(null).GetAllIn(null);
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
---
+++ SummitLog/SummitLog.Services/Services/Impl/AreaService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using SummitLog.Services.Model;
+using SummitLog.Services.Persistence;
+
+namespace SummitLog.Services.Services.Impl
+{
+    /// <summary>
+    ///     Service für Gebiete
+    /// </summary>
+    public class AreaService : IAreaService
+    {
+        private readonly IAreaDao _areaDao;
+
+        /// <summary>
+        ///     Erstellt eine neue Instanz des Service
+        /// </summary>
+        /// <param name="areaDao"></param>
+        public AreaService(IAreaDao areaDao)
+        {
+            _areaDao = areaDao;
+        }
+
+        /// <summary>
+        ///     Liefert alle Gebiete innerhalb eines Landes
+        /// </summary>
+        /// <param name="country"></param>
+        /// <returns></returns>
+        public IList<Area> GetAllIn(Country country)
+        {
+            if (country == null) throw new ArgumentNullException(nameof(country));
+            return _areaDao.GetAllIn(country);
+        }
+
+        /// <summary>
+        ///     Erstellt ein neues Gebiet innerhalb eines Landes mit dem übergebenen Namen.
+        /// </summary>
+        /// <param name="country"></param>
+        /// <param name="name"></param>
+        public void Create(Country country, string name)
+        {
+            if (country == null) throw new ArgumentNullException(nameof(country));
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+            _areaDao.Create(country, new Area {Name = name});
+        }
+    }
+}
