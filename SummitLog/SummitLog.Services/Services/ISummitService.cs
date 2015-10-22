57f405219b1ae9e8d2e8749ac7a62b22eeeb5ce9
Bernd Boruttau <berndnet2000@web.de>
2015-10-22 11:23:26 +0200

    Summit Service

---
+++ SummitLog/SummitLog.Services/Services/ISummitService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using SummitLog.Services.Model;
+
+namespace SummitLog.Services.Services
+{
+    /// <summary>
+    ///     Schnittstelle für Services für Gipfel
+    /// </summary>
+    public interface ISummitService
+    {
+        /// <summary>
+        ///     Liefert alle Gipfel innerhalb einers Gruppe
+        /// </summary>
+        /// <param name="summitGroup"></param>
+        /// <returns></returns>
+        IList<Summit> GetAllIn(SummitGroup summitGroup);
+
+        /// <summary>
+        ///     Erstellt einen neuen Gipfel innerhalb einer Gruppe mit dem übergebenen Namen.
+        /// </summary>
+        /// <param name="summitGroup"></param>
+        /// <param name="name"></param>
+        void Create(SummitGroup summitGroup, string name);
+    }
+}
---
+++ SummitLog/SummitLog.Services/Services/Impl/SummitService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using SummitLog.Services.Model;
+using SummitLog.Services.Persistence;
+
+namespace SummitLog.Services.Services.Impl
+{
+    /// <summary>
+    ///     Service für Gipfel
+    /// </summary>
+    public class SummitService : ISummitService
+    {
+        private readonly ISummitDao _summitDao;
+
+        /// <summary>
+        ///     Erstellt eine neue Instanz des Service
+        /// </summary>
+        /// <param name="summitDao"></param>
+        public SummitService(ISummitDao summitDao)
+        {
+            _summitDao = summitDao;
+        }
+
+        /// <summary>
+        ///     Liefert alle Gipfel innerhalb einer Gruppe
+        /// </summary>
+        /// <param name="summitGroup"></param>
+        /// <returns></returns>
+        public IList<Summit> GetAllIn(SummitGroup summitGroup)
+        {
+            if (summitGroup == null) throw new ArgumentNullException(nameof(summitGroup));
+            return _summitDao.GetAllIn(summitGroup);
+        }
+
+        /// <summary>
+        ///     Erstellt einen neuen Gipfel innerhalb einer Gruppe mit dem übergebenen Namen.
+        /// </summary>
+        /// <param name="summitGroup"></param>
+        /// <param name="name"></param>
+        public void Create(SummitGroup summitGroup, string name)
+        {
+            if (summitGroup == null) throw new ArgumentNullException(nameof(summitGroup));
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+            _summitDao.Create(summitGroup, new Summit {Name = name});
+        }
+    }
+}
