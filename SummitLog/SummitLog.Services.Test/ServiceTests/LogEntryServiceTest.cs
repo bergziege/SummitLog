085b1fd9fdbb5cdbf5ceaf66b1fb1a9aaf23a559
Bernd Boruttau <berndnet2000@web.de>
2015-10-23 08:27:34 +0200

    LogEntry Service

---
+++ SummitLog/SummitLog.Services.Test/ServiceTests/LogEntryServiceTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+
+using Moq;
+using NUnit.Framework;
+using SummitLog.Services.Model;
+using SummitLog.Services.Persistence;
+using SummitLog.Services.Services;
+using SummitLog.Services.Services.Impl;
+
+namespace SummitLog.Services.Test.ServiceTests
+{
+    [TestFixture]
+    public class LogEntryServiceTest
+    {
+        [Test]
+        public void TestGetAll()
+        {
+            Mock<ILogEntryDao> logDaoMock = new Mock<ILogEntryDao>();
+            logDaoMock.Setup(x => x.GetAllIn(It.IsAny<Variation>())).Returns(new List<LogEntry> {new LogEntry()});
+
+            Variation fakeVariation = new Variation();
+
+            ILogEntryService logService = new LogEntryService(logDaoMock.Object);
+            IList<LogEntry> logsInVariation = logService.GetAllIn(fakeVariation);
+            Assert.AreEqual(1, logsInVariation.Count);
+
+            logDaoMock.Verify(x=>x.GetAllIn(fakeVariation), Times.Once);
+        }
+
+        [Test]
+        public void TestCreate()
+        {
+            Mock<ILogEntryDao> logDaoMock = new Mock<ILogEntryDao>();
+            logDaoMock.Setup(x => x.Create(It.IsAny<Variation>(), It.IsAny<LogEntry>()));
+
+            Variation fakeVariation = new Variation();
+
+            ILogEntryService logService = new LogEntryService(logDaoMock.Object);
+            logService.Create("freeclimb", DateTime.Today, fakeVariation);
+
+            logDaoMock.Verify(x=>x.Create(fakeVariation, It.Is<LogEntry>(y=>y.Memo == "freeclimb" && y.DateTime == DateTime.Today)), Times.Once);
+        }
+
+        [TestCase(true,"")]
+        [TestCase(true, " ")]
+        [TestCase(true, "    ")]
+        [TestCase(true, null)]
+        [TestCase(false, "freeclimb")]
+        public void TestCreateMissingName(bool useVariation, string name)
+        {
+            Variation fakeVariation = null;
+            if (useVariation)
+            {
+                fakeVariation = new Variation();
+            }
+
+            Action act = ()=>new LogEntryService(null).Create(name, DateTime.Today, fakeVariation);
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TestGetAllWithoutVariation()
+        {
+            Action act = ()=> new LogEntryService(null).GetAllIn(null);
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
---
+++ SummitLog/SummitLog.Services/Services/ILogEntryService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using SummitLog.Services.Model;
+
+namespace SummitLog.Services.Services
+{
+    /// <summary>
+    ///     Schnittstelle für Services für Logeinträge
+    /// </summary>
+    public interface ILogEntryService
+    {
+        /// <summary>
+        ///     Liefert alle Logeinträge einer Variation
+        /// </summary>
+        /// <param name="variation"></param>
+        /// <returns></returns>
+        IList<LogEntry> GetAllIn(Variation variation);
+
+        /// <summary>
+        ///     Erstellt ein neuen Logeintrag zur Variation
+        /// </summary>
+        /// <param name="memo"></param>
+        /// <param name="date"></param>
+        /// <param name="variation"></param>
+        void Create(string memo, DateTime date, Variation variation);
+    }
+}
