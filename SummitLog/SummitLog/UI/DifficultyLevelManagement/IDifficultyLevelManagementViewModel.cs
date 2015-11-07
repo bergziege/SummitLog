243d6b18e6ce94b6a1ed9b702f9ac27008f6795f
Bernd Boruttau <berndnet2000@web.de>
2015-11-07 17:13:06 +0100

    Schwierigkeitsgradverwaltung DesignViewModels und Views

---
+++ SummitLog/SummitLog/UI/DifficultyLevelManagement/IDifficultyLevelManagementViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using SummitLog.Services.Model;
+using SummitLog.UI.Common;
+
+namespace SummitLog.UI.DifficultyLevelManagement
+{
+    /// <summary>
+    ///     Schnittstelle für View Models zur Verwaltung von Schwierigkeitsgraden
+    /// </summary>
+    public interface IDifficultyLevelManagementViewModel
+    {
+        /// <summary>
+        ///     Liefert ein Command um ein neuen Schwierigkeitsgrad hinzuzufügen
+        /// </summary>
+        RelayCommand AddDifficultyLevelCommand { get; }
+
+        /// <summary>
+        ///     Liefert die Liste aller Schwierigkeitsgrade
+        /// </summary>
+        ObservableCollection<DifficultyLevel> DifficultyLevels { get; }
+
+        /// <summary>
+        ///     LÄdt die VM relevanten Daten zu einer Schwierigkeitsgradskale
+        /// </summary>
+        /// <param name="difficultyLevelScale"></param>
+        void LoadData(DifficultyLevelScale difficultyLevelScale);
+    }
+}
---
+++ SummitLog/SummitLog/UI/DifficultyLevelScaleManagement/DesignViewModels/DifficultyLevelScaleManagementDesignViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
+using SummitLog.Services.Model;
+using SummitLog.UI.Common;
+
+namespace SummitLog.UI.DifficultyLevelScaleManagement.DesignViewModels
+{
+    /// <summary>
+    /// Design View Model einer Ansicht zur Verwaltung der Schwierigkeitsgradskalen
+    /// </summary>
+    public class DifficultyLevelScaleManagementDesignViewModel: ReactiveObject, IDifficultyLevelScaleManagementViewModel
+    {
+        /// <summary>
+        /// Liefert eine neue Instanz des Design View Models
+        /// </summary>
+        public DifficultyLevelScaleManagementDesignViewModel()
+        {
+            DifficultyLevelScales.Add(new DifficultyLevelScale() {Name = "Weltweit"});
+            DifficultyLevelScales.Add(new DifficultyLevelScale() {Name = "Sächsich"});
+        }
+
+        /// <summary>
+        ///     LIefert ein Command um eine neue Schwierigkeitsgradskala hinzuzufügen
+        /// </summary>
+        public RelayCommand AddDifficultyLevelScaleCommand { get; }
+
+        /// <summary>
+        ///     Liefert die Liste der Schwierigkeitsgradskalen
+        /// </summary>
+        public ObservableCollection<DifficultyLevelScale> DifficultyLevelScales { get; } =new ObservableCollection<DifficultyLevelScale>();
+
+        /// <summary>
+        ///     Liefert oder setzt die gewählte Schwierigkeitsgradskala
+        /// </summary>
+        public DifficultyLevelScale SelectedDifficultyLevelScale { get; set; }
+
+        /// <summary>
+        ///     Lädt die VM relevanten Daten
+        /// </summary>
+        public void LoadData()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
