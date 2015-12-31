e41540614210590cf6760a6f14f06829c3d32436
Bernd Boruttau <berndnet2000@web.de>
2015-12-31 15:37:09 +0100

    formatting

--- SummitLog/SummitLog.Services.Test/ServiceTests/VariationServiceTest.cs
+++ SummitLog/SummitLog.Services.Test/ServiceTests/VariationServiceTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using FluentAssertions;
-using FluentAssertions.Common;
 using Moq;
 using NUnit.Framework;
 using SummitLog.Services.Exceptions;
@@ -15,36 +14,6 @@ namespace SummitLog.Services.Test.ServiceTests
     [TestFixture]
     public class VariationServiceTest
     {
-        [Test]
-        public void TestGetAllOnRoute()
-        {
-            Mock<IVariationDao> variationDaoMock = new Mock<IVariationDao>();
-            variationDaoMock.Setup(x => x.GetAllOn(It.IsAny<Route>())).Returns(new List<Variation> {new Variation() {Name = "Gebiet 1"}});
-
-            Route fakeRoute = new Route() {Name = "D"};
-
-            IVariationService variationService = new VariationService(variationDaoMock.Object);
-            IList<Variation> variationsOnRoute = variationService.GetAllOn(fakeRoute);
-            Assert.AreEqual(1, variationsOnRoute.Count);
-
-            variationDaoMock.Verify(x=>x.GetAllOn(fakeRoute), Times.Once);
-        }
-
-        [Test]
-        public void TestCreate()
-        {
-            Mock<IVariationDao> variationDaoMock = new Mock<IVariationDao>();
-            variationDaoMock.Setup(x => x.Create(It.IsAny<Variation>(), It.IsAny<Route>(), It.IsAny<DifficultyLevel>()));
-
-            Route fakeRoute = new Route();
-            DifficultyLevel fakeLevel = new DifficultyLevel();
-
-            IVariationService variationService = new VariationService(variationDaoMock.Object);
-            variationService.Create("Variation 1", fakeRoute, fakeLevel);
-
-            variationDaoMock.Verify(x=>x.Create(It.Is<Variation>(y=>y.Name == "Variation 1"), fakeRoute, fakeLevel), Times.Once);
-        }
-
         [TestCase(null, true, true)]
         [TestCase("", true, true)]
         [TestCase(" ", true, true)]
@@ -64,14 +33,7 @@ namespace SummitLog.Services.Test.ServiceTests
                 level = new DifficultyLevel();
             }
 
-            Action act = ()=>new VariationService(null).Create(name, route, level);
-            act.ShouldThrow<ArgumentNullException>();
-        }
-
-        [Test]
-        public void TestGetAllWithoutRoute()
-        {
-            Action act = ()=> new VariationService(null).GetAllOn(null);
+            Action act = () => new VariationService(null).Create(name, route, level);
             act.ShouldThrow<ArgumentNullException>();
         }
 
@@ -88,15 +50,54 @@ namespace SummitLog.Services.Test.ServiceTests
             IVariationService service = new VariationService(variationDaoMock.Object);
             service.Delete(variation);
 
-            variationDaoMock.Verify(x=>x.IsInUse(variation), Times.Once);
-            variationDaoMock.Verify(x=>x.Delete(variation), Times.Once);
+            variationDaoMock.Verify(x => x.IsInUse(variation), Times.Once);
+            variationDaoMock.Verify(x => x.Delete(variation), Times.Once);
+        }
+
+        [Test]
+        public void TestCreate()
+        {
+            Mock<IVariationDao> variationDaoMock = new Mock<IVariationDao>();
+            variationDaoMock.Setup(x => x.Create(It.IsAny<Variation>(), It.IsAny<Route>(), It.IsAny<DifficultyLevel>()));
+
+            Route fakeRoute = new Route();
+            DifficultyLevel fakeLevel = new DifficultyLevel();
+
+            IVariationService variationService = new VariationService(variationDaoMock.Object);
+            variationService.Create("Variation 1", fakeRoute, fakeLevel);
+
+            variationDaoMock.Verify(
+                x => x.Create(It.Is<Variation>(y => y.Name == "Variation 1"), fakeRoute, fakeLevel), Times.Once);
+        }
+
+        [Test]
+        public void TestGetAllOnRoute()
+        {
+            Mock<IVariationDao> variationDaoMock = new Mock<IVariationDao>();
+            variationDaoMock.Setup(x => x.GetAllOn(It.IsAny<Route>()))
+                .Returns(new List<Variation> {new Variation {Name = "Gebiet 1"}});
+
+            Route fakeRoute = new Route {Name = "D"};
+
+            IVariationService variationService = new VariationService(variationDaoMock.Object);
+            IList<Variation> variationsOnRoute = variationService.GetAllOn(fakeRoute);
+            Assert.AreEqual(1, variationsOnRoute.Count);
+
+            variationDaoMock.Verify(x => x.GetAllOn(fakeRoute), Times.Once);
+        }
+
+        [Test]
+        public void TestGetAllWithoutRoute()
+        {
+            Action act = () => new VariationService(null).GetAllOn(null);
+            act.ShouldThrow<ArgumentNullException>();
         }
 
         [Test]
         public void TestIsInUse()
         {
             Mock<IVariationDao> variationDaoMock = new Mock<IVariationDao>();
-        variationDaoMock.Setup(x => x.IsInUse(It.IsAny<Variation>())).Returns(true);
+            variationDaoMock.Setup(x => x.IsInUse(It.IsAny<Variation>())).Returns(true);
 
             Variation variation = new Variation();
 
