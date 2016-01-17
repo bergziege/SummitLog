c5f943626cdda11cb5d9336cfc700e0a87fbe076
Bernd Boruttau <berndnet2000@web.de>
2016-01-17 11:19:19 +0100

    [SUMMITLOG-27] - Focus auf erstes Element in Eingabefenstern

--- SummitLog/SummitLog/UI/LogEntryInput/LogEntryInputView.xaml.cs
+++ SummitLog/SummitLog/UI/LogEntryInput/LogEntryInputView.xaml.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SummitLog.UI.LogEntryInput
 {
     /// <summary>
-    /// Interaction logic for LogEntryInputView.xaml
+    ///     Interaction logic for LogEntryInputView.xaml
     /// </summary>
     public partial class LogEntryInputView : Window
     {
         public LogEntryInputView()
         {
             InitializeComponent();
+
+            Loaded += (sender, e) =>
+                MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
         }
     }
 }
